827f6f64317270f7769faeb45d70c6251b482ee7
manojsid <siddammanoj1998@gmail.com>
2020-06-12 16:41:26 +0530

    Dictionary Intailized

--- arth.sh
+++ arth.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 echo "Welcome to arthimetic code"
+declare -A Operation
+
 
 read -p "Enter the a value: " a
 read -p "Enter the b value: " b
 read -p "Enter the c value: " c
 
 result1=$((($a+$b)*$c))
-echo $result1
+Operation[op1]=$result1
+
 
 result2=$((($a*$b)+$c))
-echo $result2
+Operation[op2]=$result2
+#echo $result2
 
 result3=$((($c+$a)/$b))
-echo $result3
+Operation[op3]=$result3
+#echo $result3
 
 result4=$((($a%$b)+$c))
-echo $result4
+Operation[op4]=$result4
+
+echo ${Operation[@]}
