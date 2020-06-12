3fc08b49e7cf405577699f007a00158fcccfc792
manojsid <siddammanoj1998@gmail.com>
2020-06-12 16:22:51 +0530

    Added 2nd operation

--- arth.sh
+++ arth.sh
@@ -5,7 +5,9 @@ read -p "Enter the a value: " a
 read -p "Enter the b value: " b
 read -p "Enter the c value: " c
 
+result1=$((($a+$b)*$c))
 
-result=$((($a+$b)*$c))
+echo $result1
 
-echo $result
+result2=$((($a*$b)+$c))
+echo $result2
